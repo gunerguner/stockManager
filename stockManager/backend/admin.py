9fb767ba96819f1eaf51aae09433a808004399f4
Zhicheng Zhang <robin.zhangzc@koubei.com>
2021-05-13 19:21:18 +0800

    stockMeta model

--- stockManager/backend/admin.py
+++ stockManager/backend/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Operation,Info
+from .models import Operation,Info,StockMeta
 
 admin.site.register(Operation)
 admin.site.register(Info)
+admin.site.register(StockMeta)
